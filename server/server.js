40238ed61055341748c19508883b7cc6029adde6
msoncsswe <miris11260@gmail.com>
2023-06-13 15:45:15 -0400

    server/server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
+const Socket = require('websocket').server;
 const express = require('express');
 const path = require('path');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 const io = new Server(server);
 
 app.use('/', express.static(path.join(__dirname, '../build')));
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+  console.log('a user connected');
 });
 
 server.listen(3000, () => {
-    console.log('listening on PORT:3000');
+  console.log('listening on PORT:3000');
 });
