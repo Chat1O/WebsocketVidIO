174b04367bed2683ad2392b8ade45a7008fb2c34
Andrew <androoo.1997@gmail.com>
2023-06-14 13:27:31 -0400

    fixed page infinitely reloading

--- Client/Components/Home.jsx
+++ Client/Components/Home.jsx
@@ -3,6 +3,7 @@ import io from 'socket.io-client';
 import User from './User';
 import UserList from './userList';
 
+const socket = io('http://localhost:3000');
 
 export default function Home() {
   const [connectbtn, setconnectbtn] = useState(true);
@@ -11,10 +12,19 @@ export default function Home() {
   const [activeUsers, setActiveUsers] = useState([]);
   const [test, setTest] = useState('');
 
-  //const socket = io('http://localhost:3000');
-
   function connectButtonHit() {
-    setconnectbtn(cur => !cur);
+    if (connectbtn) {
+      socket.connect(); // Connect the WebSocket if it's not already connected
+      setconnectbtn(false);
+      console.log('connecting');
+    } else {
+      socket.disconnect(); // Disconnect the WebSocket
+      setconnectbtn(true);
+      console.log('disconnecting');
+      if (localVideoRef.current) {
+        localVideoRef.current.srcObject = null; // Clear the video stream
+      }
+    }
   }
 
   function handleUserSelected(socketId) {
@@ -23,60 +33,64 @@ export default function Home() {
 
   // on component mount, display local video
   useEffect(() => {
-    // checkif browser supports the media devices api
+    // check if the browser supports the media devices API
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({ video: true })
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
         .then((stream) => {
           // attach the video stream
-          if (localVideoRef.current) {
+          if (localVideoRef.current && connectbtn === false) {
             localVideoRef.current.srcObject = stream;
           }
         })
         .catch((err) => {
           console.log(err);
-        })
+        });
     }
     // cleanup function to stop the video stream
     return () => {
       if (localVideoRef.current && localVideoRef.current.srcObject) {
-        localVideoRef.current.srcObject.getTracks().forEach(track => track.stop());
+        localVideoRef.current.srcObject.getTracks().forEach((track) => track.stop());
       }
-    }
-  }, []);
+    };
+  }, [connectbtn]);
 
   // event listeners for socket.io
   useEffect(() => {
-    const socket = io('http://localhost:3000');
     socket.on('update-user-list', ({ users }) => {
       setActiveUsers(users);
-      console.log(activeUsers)
-    })
+      console.log(activeUsers);
+    });
 
     socket.on('remove-user', ({ socketId }) => {
-      setActiveUsers(activeUsers.filter(cur => cur === socketId));
-    })
+      setActiveUsers(activeUsers.filter((cur) => cur === socketId));
+    });
 
     return () => {
-      socket.disconnect();
-    }
-  },[]);
+      // No need to disconnect the socket here
+    };
+  }, []);
 
   return (
     <div className="w-screen h-screen bg-blue-100 flex justify-center items-center">
-      <UserList users={activeUsers} handleUserSelected={handleUserSelected}/>
-      
+      <UserList users={activeUsers} handleUserSelected={handleUserSelected} />
+
       <div className="card w-2/3 h-2/3 shadow-xl border bg-base-100 flex flex-col justify-center items-center">
         <div className="flex justify-between">
-          <video className="h-2/3" id="localVideo" ref={localVideoRef} autoPlay/>
-          <video className="h-2/3" id="remoteVideo" autoPlay/>
+          <video className="h-2/3" id="localVideo" ref={localVideoRef} autoPlay />
+          <video className="h-2/3" id="remoteVideo" autoPlay />
         </div>
-        <p className="max-w-lg text-2xl font-semibold leading-loose text-gray-900 dark:text-white justify-self-center" >Hello, Welcome to WebsocketVid.io</p>
+        <p className="max-w-lg text-2xl font-semibold leading-loose text-gray-900 dark:text-white justify-self-center">
+          Hello, Welcome to WebsocketVid.io
+        </p>
         <div className="flex">
-          <button className="btn" onClick={connectButtonHit}>{connectbtn ? 'Connect' : 'Disconnect'}</button>
+          <button className="btn" onClick={connectButtonHit}>
+            {connectbtn ? 'Connect' : 'Disconnect'}
+          </button>
           <button className="btn">send message</button>
           <p>Selected: {test}</p>
         </div>
       </div>
     </div>
-  )
+  );
 }
